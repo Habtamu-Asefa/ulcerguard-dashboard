6b9b526f5e42c2e436a2d176bb0773d09003789d
Habtamu Asefa <nazrihabtish@gmail.com>
2024-05-15 01:49:30 +0300

    Initial customization

---
+++ src/components/sidebar/components/Brand.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+// Chakra imports
+import { Flex, Image, useColorModeValue } from "@chakra-ui/react";
+
+// Custom components
+import { HorizonLogo } from "components/icons/Icons";
+import { HSeparator } from "components/separator/Separator";
+import Logo from "assets/img/Logo.png";
+
+export function SidebarBrand() {
+  //   Chakra color mode
+  let logoColor = useColorModeValue("navy.700", "white");
+
+  return (
+    <Flex align="center" direction="column">
+      {/* <HorizonLogo h="26px" w="175px" my="32px" color={logoColor} /> */}
+      <Image
+        boxSize="100px"
+        objectFit="cover"
+        borderRadius={"50px"}
+        src={Logo}
+        alt=""
+      />
+      <HSeparator mb="20px" mt="20px" />
+    </Flex>
+  );
+}
+
+export default SidebarBrand;
---
+++ src/views/admin/marketplace/index.jsx
@@ -0,0 +1,55 @@
+/*!
+  _   _  ___  ____  ___ ________  _   _   _   _ ___   
+ | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
+ | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
+ |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
+ |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
+                                                                                                                                                                                                                                                                                                                                       
+=========================================================
+* Horizon UI - v1.1.0
+=========================================================
+
+* Product Page: https://www.horizon-ui.com/
+* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
+
+* Designed and Coded by Simmmple
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+import React from "react";
+
+// Chakra imports
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Link,
+  Text,
+  useColorModeValue,
+  SimpleGrid,
+} from "@chakra-ui/react";
+
+// Custom components
+import ColumnsTable from "../dataTables/components/ColumnsTable";
+import { columnsDataColumns } from "../dataTables/variables/columnsData";
+import tableDataColumns from "views/admin/dataTables/variables/tableDataColumns.json";
+
+export default function Marketplace() {
+  // Chakra Color Mode
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const textColorBrand = useColorModeValue("brand.500", "white");
+  return (
+    <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
+      {/* Main Fields */}
+      <ColumnsTable
+        columnsData={columnsDataColumns}
+        tableData={tableDataColumns}
+      />
+    </Box>
+  );
+}
